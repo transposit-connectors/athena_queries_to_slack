b2ae7deea8cedecca07b4224793f0f4a21d8c57b
yoko <yoko@transposit.com>
2019-05-21 21:14:43 +0000

    Commit from Transposit IDE

--- src/hello_world.js
+++ src/hello_world.js
@@ -1,14 +1,18 @@
 (params) => {
-  return [
-    {
-      language: "english",
-      message: "Hello, world"
-    },
-    {
-      language: "spanish",
-      message: "Hola, mundo"
-    }
-  ];
+	const query = params.query;
+  	let executionId = api.run("aws_athena.start_query_execution", {
+      QueryString : params.query,
+      ClientRequestToken: makeid(35),
+      WorkGroup: "primary",
+      ResultConfiguration: {OutputLocation: 's3://prod.events-logs.transposit.com/query-results'}
+    })[0]['QueryExecutionId'];
+  	//return api.run("this.create_execution_record", {id: executionId, tableName: 'new_signup_executionIds'});
+  console.log(executionId)
+  	return api.run('aws_dynamodb.put_item', {Item: 
+                                             {event_type: {"S": "yesterday_signup_execution_id"}, 
+                                              execution_id: {"S": executionId},
+                                              daysAgo: {"N": (params.daysAgo).toString()}
+                                             }, TableName: 'events'});
 }
 
 /*
---
+++ src/makeId.js
@@ -0,0 +1,15 @@
+params => {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < params.length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
+
+/*
+ * For sample code and reference material, visit
+ * https://docs.transposit.com/references/js-operations
+ */
